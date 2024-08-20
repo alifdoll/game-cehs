d3772810ee1eea081d883f8678e8fd620fb3646f
Yudhistira Anggara Jayadinata <72541782+yuira34@users.noreply.github.com>
2024-08-20 19:56:26 +0700

    Final build

--- scripts/Tetroids.cs
+++ scripts/Tetroids.cs
@@ -1,26 +1,17 @@
 using Godot;
 using System;
 
-public partial class Main : Control
+public partial class Tetroids : RigidBody2D
 {
-	[Export]
-	public Node2D tetroidPlace;
-	[Export]
-	public int targetCount = 5;	
-	public int tetroidCount;
+	public bool isSelected = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		tetroidCount = 0;
+		Transform2D transform2d = new Transform2D();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GD.Print(tetroidCount);
-		if(tetroidCount >= targetCount)
-		{
-			GD.Print("You Win!");
-		}
 	}
 }
